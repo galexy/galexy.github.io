1da7a4a4db48deabec24ef0c8161f5a3bc0d511e
Galex Yen <galex@reasoning.net>
2016-05-28 23:09:17 -0700

    Port over lanyon theme

--- site.hs
+++ site.hs
@@ -1,6 +1,8 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+import           Data.Monoid    (mappend)
+import           Data.Monoid    ((<>))
+import           Control.Monad  (liftM)
 import           Hakyll
 
 
@@ -12,57 +14,76 @@ main = hakyll $ do
         compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+        route $ idRoute
+        compile $ compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match "pages/*" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/page.html" siteCtx
+            >>= loadAndApplyTemplate "templates/default.html" siteCtx
+            >>= relativizeUrls
+
+    match "pages/*" $ version "titleLine" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+
+    match "404.html" $ do
+        route $ idRoute
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" siteCtx
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
+    blog <- buildPaginateWith grouper "posts/*"
+        (\n -> if n == 1
+            then "index.html"
+            else fromCapture "page/*.html" (show n))
+
+    paginateRules blog $ \pageNum pattern -> do
+        -- Copied from posts, need to refactor
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+            posts <- recentFirst =<< loadAllSnapshots pattern "content"
+            let paginateCtx = paginateContext blog pageNum
+            let ctx         =
+                    constField "title" "Blog"                  <>
+                    listField "posts" teaserCtx (return posts) <>
+                    paginateCtx                                <>
+                    siteCtx
 
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= loadAndApplyTemplate "templates/blog.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
+teaserCtx :: Context String
+teaserCtx = teaserField "teaser" "content" `mappend` postCtx
+
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+    siteCtx
 
+siteCtx :: Context String
+siteCtx = 
+  listField "pages" postCtx (loadAll ("pages/*" .&&. hasVersion "titleLine"))  `mappend`
+  constField "site-title" "Galex's Blog"         `mappend`
+  constField "site-tagline" "Huh?"               `mappend`
+  constField "site-description" "Geek blog"      `mappend`
+  defaultContext
+
+-- Run sortRecentFirst on ids, and then liftM (paginateEvery 3) into it
+grouper :: MonadMetadata m => [Identifier] -> m [[Identifier]]
+grouper ids = (liftM (paginateEvery 3) . sortRecentFirst) ids
