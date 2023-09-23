8b1c026d5b034a21a97eda1a50dac222a8e49b33
Galex Yen <galexy@users.noreply.github.com>
2023-09-22 20:36:12 -0700

    Update projects data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,12 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
+    title: 'StreamInsight',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+    href: 'https://learn.microsoft.com/en-us/archive/msdn-magazine/2012/march/microsoft-streaminsight-building-the-internet-of-things',
+  }
 ]
 
 export default projectsData
