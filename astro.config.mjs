8c9068ae698b3c7c1dc6649f93fc5c63d065514b
Galex Yen <galexy@users.noreply.github.com>
2025-12-26 10:27:51 -0800

    Port over terminal theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  }
+});
